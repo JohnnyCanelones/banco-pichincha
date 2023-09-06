8625a38722ccf05848fc0e991193fc55d77803d2
johnny <desarrollo3@pcnub.com>
2023-09-06 17:21:15 -0500

    routing module setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './product/product.component';
+const routes: Routes = [
+  { path: '', component: ProductComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    FormsModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

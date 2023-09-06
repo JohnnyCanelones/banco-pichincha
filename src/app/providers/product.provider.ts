418e5d34e56770436c43d1a718fee839eab44a74
johnny <desarrollo3@pcnub.com>
2023-09-06 17:21:59 -0500

    product provider

---
+++ src/app/providers/product.provider.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductProvider {
+  private baseUrl = 'https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros';
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'authorId': '123456789',
+  });
+  
+  constructor(private http: HttpClient) {}
+
+ 
+
+  getProducts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/bp/products`, { headers: this.headers });
+  }
+
+  getProduct( productId: string): Observable<boolean> {
+    return this.http.get<boolean>(`${this.baseUrl}/bp/products/verification?id=${productId}`, { headers: this.headers });
+  }
+
+  createProducts( product: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/bp/products`, product, { headers: this.headers });
+  }
+
+  updatesProducts( product: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/bp/products`, product, { headers: this.headers });
+  }
+
+  deleteProducts( productId: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/bp/products?id=${productId}`, { headers: this.headers });
+  }
+
+}
